77ff858e10e1e00f4fe8e67c8aae0761a5548ee1
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-02-01 13:24:05 +0600

    Added blog post count navigation badge

--- app/Filament/Resources/BlogPostResource.php
+++ app/Filament/Resources/BlogPostResource.php
@@ -19,6 +19,11 @@ class BlogPostResource extends Resource
 
     protected static ?int $navigationSort = 4;
 
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
