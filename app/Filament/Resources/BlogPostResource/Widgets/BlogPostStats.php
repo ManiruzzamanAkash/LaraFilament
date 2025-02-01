6e382847cbf43de6b8244460b0d04c65eba39eb7
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-02-01 10:59:09 +0600

    Number format on Blog post stats

--- app/Filament/Resources/BlogPostResource/Widgets/BlogPostStats.php
+++ app/Filament/Resources/BlogPostResource/Widgets/BlogPostStats.php
@@ -10,14 +10,18 @@ class BlogPostStats extends BaseWidget
 {
     protected function getStats(): array
     {
+        $totalPublishedBlogPosts = BlogPost::where('is_published', true)->count();
+        $totalDraftBlogPosts = BlogPost::where('is_published', false)->count();
+        $totalViews = BlogPost::sum('views');
+
         return [
-            Stat::make('Published Posts', value: BlogPost::where('is_published', true)->count())
+            Stat::make('Published Posts', number_format($totalPublishedBlogPosts))
                 ->icon('heroicon-o-document-text')
                 ->color('blue'),
-            Stat::make('Draft Posts', value: BlogPost::where('is_published', false)->count())
+            Stat::make('Draft Posts', number_format($totalDraftBlogPosts))
                 ->icon('heroicon-o-document')
                 ->color('gray'),
-            Stat::make('Total Views', value: BlogPost::sum('views'))
+            Stat::make('Total Views', number_format($totalViews))
                 ->icon('heroicon-o-eye')
                 ->color('green'),
         ];
